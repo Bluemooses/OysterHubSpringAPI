f227fa22f07f6d0fadd4e668575a80a9d32e138c
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-25 10:02:19 -0600

    Using scanner to add input values to program

--- src/main/java/com/example/oysterhub/OysterhubApplication.java
+++ src/main/java/com/example/oysterhub/OysterhubApplication.java
@@ -7,14 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 @RestController
 
 public class OysterhubApplication implements CommandLineRunner {
+    //allows us to type input into the console.
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         SpringApplication.run(OysterhubApplication.class, args);
         int[] myIntArray = new int[10];
+        int[] myIntegers = getIntegers(5);
 
         for(int i = 0; i<myIntArray.length; i++){
             myIntArray[i] = i * 10;
@@ -22,7 +27,21 @@ public class OysterhubApplication implements CommandLineRunner {
 
         printArray(myIntArray);
 
+        for(int i=0; i<myIntegers.length; i++){
+            System.out.println("Element " + i + " typed values is: " + myIntegers[i]);
+        }
 
+    }
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i = 0; i<values.length; i++){
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
     }
 
     public static void printArray(int[] array){
